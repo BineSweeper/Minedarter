9a766cdaf06c8a120f4727fe76464c691c7bb36b
SD <harrypotter.starwars.shrish@gmail.com>
2022-11-11 10:54:56 +0530

    Project

---
+++ bin/minedarter.dart
@@ -0,0 +1,5 @@
+import 'package:minedarter/minedarter.dart' as minedarter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${minedarter.calculate()}!');
+}
---
+++ test/minedarter_test.dart
@@ -0,0 +1,8 @@
+import 'package:minedarter/minedarter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
